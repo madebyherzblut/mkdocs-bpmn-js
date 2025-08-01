0cb848674a7fd496008c58ab728e02d181740643
Christian Schuhmann <97274+madebyherzblut@users.noreply.github.com>
2025-08-01 18:43:15 +0200

    feat: Add image rendering

--- README.md
+++ README.md
@@ -29,12 +29,14 @@ plugins:
       class: "mk-bpmn-js"
 ```
 
-| Option              | Description                                  | Default Value                                                                   |
-| ------------------- | -------------------------------------------- | ------------------------------------------------------------------------------- |
-| `viewer_js`         | URL to the BPMN viewer JavaScript file.      | `https://unpkg.com/bpmn-js@18/dist/bpmn-navigated-viewer.production.min.js`     |
-| `viewer_css`        | URL to the BPMN viewer CSS file.             | `https://unpkg.com/bpmn-js@18/dist/assets/bpmn-js.css`                          |
-| `viewer_initialize` | Append a script to load the diagrams.        | `True`                                                                          |
-| `class`             | CSS class applied to each diagram container. | `mk-bpmn-js`                                                                    |
+| Option              | Description                                          | Default Value                                                               |
+| ------------------- | ---------------------------------------------------- | --------------------------------------------------------------------------- |
+| `render`            | Specify the render mode (`none`, `image`, `viewer`). | `viewer`                                                                    |
+| `class`             | CSS class applied to each diagram container.         | `mk-bpmn-js`                                                                |
+| `viewer_js`         | URL to the BPMN viewer JavaScript file.              | `https://unpkg.com/bpmn-js@18/dist/bpmn-navigated-viewer.production.min.js` |
+| `viewer_css`        | URL to the BPMN viewer CSS file.                     | `https://unpkg.com/bpmn-js@18/dist/assets/bpmn-js.css`                      |
+| `viewer_initialize` | Append a script to load the diagrams.                | `True`                                                                      |
+| `image_command`     | Command to call when `render` is set to `image`.     | `bpmn-to-image --no-title --no-footer $input:$output`                       |
 
 ### Local viewer assets
 
@@ -50,16 +52,10 @@ Override the default theme to include your local assets by creating a custom `th
 Refer to the [MkDocs guide on customizing themes](https://www.mkdocs.org/user-guide/customizing-your-theme/#overriding-template-blocks) for more details.
 
 ```html
-{% extends "base.html" %}
-
-{% block styles %}
-  {{ super() }}
-  <link rel="stylesheet" href="{{ base_url }}/css/bpmn.css" />
-{% endblock %}
-
-{% block libs %}
-  {{ super() }}
-  <script src="{{ base_url }}/js/bpmn.js"></script>
+{% extends "base.html" %} {% block styles %} {{ super() }}
+<link rel="stylesheet" href="{{ base_url }}/css/bpmn.css" />
+{% endblock %} {% block libs %} {{ super() }}
+<script src="{{ base_url }}/js/bpmn.js"></script>
 {% endblock %}
 ```
 
@@ -78,15 +74,19 @@ Add `.bpmn` files using standard Markdown image syntax:
 
 ```markdown
 <!-- Relative path to the current Markdown file -->
+
 ![Hello World](hello-world.bpmn)
 
 <!-- Absolute path from the site root -->
+
 ![Hello World](/diagrams/hello-world.bpmn)
 
 <!-- No alternative text -->
+
 ![](hello-world.bpmn)
 
 <!-- With diagram options (see below for more details) -->
+
 ![](hello-world.bpmn?id=hello-world&height=400px)
 ```
 
@@ -108,5 +108,5 @@ You can customize individual diagrams using query parameters in the image URL.
 
 ## Acknowledgments
 
-* Inspired by [mkdocs-drawio](https://github.com/tuunit/mkdocs-drawio), which served as a helpful reference for embedding diagrams in MkDocs.
-* Also check out [mkdocs-bpmn](https://github.com/vanchaxy/mkdocs-bpmn), an alternative implementation. Depending on your needs, it might be a better fit.
+- Inspired by [mkdocs-drawio](https://github.com/tuunit/mkdocs-drawio), which served as a helpful reference for embedding diagrams in MkDocs.
+- Also check out [mkdocs-bpmn](https://github.com/vanchaxy/mkdocs-bpmn), an alternative implementation. Depending on your needs, it might be a better fit.
--- src/mkdocs_bpmn_js/plugin.py
+++ src/mkdocs_bpmn_js/plugin.py
@@ -1,11 +1,16 @@
+import hashlib
+import os
 import re
+import shlex
+import subprocess
+from pathlib import Path
+from urllib.parse import parse_qs, urlparse
 
 from bs4 import BeautifulSoup
 from mkdocs.config import config_options
+from mkdocs.exceptions import PluginError
 from mkdocs.plugins import BasePlugin, get_plugin_logger
-
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
+from mkdocs.utils import copy_file
 
 log = get_plugin_logger(__name__)
 
@@ -16,6 +21,12 @@ class BPMNPlugin(BasePlugin):
     """
 
     config_scheme = (
+        (
+            "render",
+            config_options.Choice(["none", "image", "viewer"], default="viewer"),
+        ),
+        ("class", config_options.Type(str, default="mk-bpmn-js")),
+        ("cache_dir", config_options.Type(str, default=".bpmn-cache")),
         (
             "viewer_js",
             config_options.Type(
@@ -31,8 +42,37 @@ class BPMNPlugin(BasePlugin):
             ),
         ),
         ("viewer_initialize", config_options.Type(bool, default=True)),
-        ("class", config_options.Type(str, default="mk-bpmn-js")),
+        (
+            "image_command",
+            config_options.Type(
+                str,
+                default="bpmn-to-image --no-title --no-footer $input:$output",
+            ),
+        ),
     )
+
+    render_queue = None
+
+    def on_config(self, config):
+        if self.config["render"] == "image" and not self.config["image_command"]:
+            raise PluginError(
+                "Render mode 'image' requires a 'image_command' to be specified."
+            )
+
+        if self.config["cache_dir"]:
+            cache_dir = Path(self.config["cache_dir"])
+            cache_dir.mkdir(parents=True, exist_ok=True)
+
+        if (
+            self.config["image_command"]
+            and "$input" not in self.config["image_command"]
+        ):
+            raise PluginError("Missing '$input' placeholder in image_command.")
+        if (
+            self.config["image_command"]
+            and "$output" not in self.config["image_command"]
+        ):
+            raise PluginError("Missing '$output' placeholder in image_command.")
 
     def on_post_page(self, output, config, page, **kwargs):
         if ".bpmn" not in output:
@@ -47,6 +87,7 @@ class BPMNPlugin(BasePlugin):
             return output
 
         used_ids = set()
+        self.render_queue = {}
 
         for idx, diagram in enumerate(diagrams):
             log.debug(f"Embed diagram '{diagram['src']}' in page '{page.title}'")
@@ -58,22 +99,39 @@ class BPMNPlugin(BasePlugin):
                 params = parse_qs(parsed_url.query)
                 src = parsed_url.path
 
-            tag = html.new_tag("span")
-            tag.attrs["class"] = self.config["class"]
-            tag.attrs["data-src"] = src
+            if self.config["render"] == "none":
+                diagram.decompose()
+                continue
 
-            if diagram["alt"]:
-                tag.attrs["data-alt"] = diagram["alt"]
+            tag = None
 
-                link = html.new_tag("a")
-                link.attrs["href"] = src
-                link.attrs["download"] = ""
-                link.append(diagram["alt"])
+            if self.config["render"] == "image":
+                input_src = Path(config["docs_dir"]) / src
+                file_hash = compute_file_hash(input_src)
 
-                noscript = html.new_tag("noscript")
-                noscript.append(link)
+                tag = html.new_tag("img")
+                tag.attrs["class"] = self.config["class"]
+                tag.attrs["data-src"] = src
+                tag.attrs["src"] = src.replace(".bpmn", ".svg?" + file_hash)
 
-                tag.append(noscript)
+                self.render_queue[src] = file_hash
+            elif self.config["render"] == "viewer":
+                tag = html.new_tag("span")
+                tag.attrs["class"] = self.config["class"]
+                tag.attrs["data-src"] = src
+
+                if diagram["alt"]:
+                    tag.attrs["data-alt"] = diagram["alt"]
+
+                    link = html.new_tag("a")
+                    link.attrs["href"] = src
+                    link.attrs["download"] = ""
+                    link.append(diagram["alt"])
+
+                    noscript = html.new_tag("noscript")
+                    noscript.append(link)
+
+                    tag.append(noscript)
 
             if "id" in params:
                 tag.attrs["id"] = params["id"][0]
@@ -96,7 +154,7 @@ class BPMNPlugin(BasePlugin):
 
             diagram.replace_with(tag)
 
-        if self.config["viewer_css"]:
+        if self.config["render"] == "viewer" and self.config["viewer_css"]:
             link_viewer = html.new_tag(
                 "link",
                 rel="stylesheet",
@@ -105,11 +163,11 @@ class BPMNPlugin(BasePlugin):
             )
             html.head.append(link_viewer)
 
-        if self.config["viewer_js"]:
+        if self.config["render"] == "viewer" and self.config["viewer_js"]:
             script_viewer = html.new_tag("script", src=self.config["viewer_js"])
             html.body.append(script_viewer)
 
-        if self.config["viewer_initialize"]:
+        if self.config["render"] == "viewer" and self.config["viewer_initialize"]:
             script = html.new_tag("script", type="text/javascript")
             script.string = """
                 document.addEventListener('DOMContentLoaded', async function() {
@@ -142,3 +200,53 @@ class BPMNPlugin(BasePlugin):
             html.body.append(script)
 
         return str(html)
+
+    def on_post_build(self, *, config):
+        if not self.config["render"] == "image" or not self.render_queue:
+            return
+
+        docs_dir = Path(config["docs_dir"])
+        site_dir = Path(config["site_dir"])
+        cache_dir = Path(self.config["cache_dir"])
+
+        for file_src, file_hash in self.render_queue.items():
+            input_src = Path(file_src)
+
+            input_file = docs_dir / input_src
+            cache_file = cache_dir / f"{file_hash}.svg"
+            output_file = site_dir / input_src.with_suffix(".svg")
+
+            if not cache_file.exists():
+                command = (
+                    self.config["image_command"]
+                    .replace("$input", str(input_file))
+                    .replace("$output", str(cache_file))
+                )
+
+                try:
+                    log.debug(f"Render diagram with: {command}")
+                    subprocess.run(
+                        shlex.split(command),
+                        capture_output=True,
+                        text=True,
+                        check=True,
+                        env=os.environ,
+                    )
+                except subprocess.CalledProcessError as e:
+                    raise PluginError(f"Subprocess failed with error: {e.stderr}")
+
+            copy_file(cache_file, output_file)
+
+        self.render_queue = None
+
+
+def compute_file_hash(path):
+    """
+    Compute a hash for the file at the given path.
+    """
+
+    hasher = hashlib.md5()
+    with open(path, "rb") as f:
+        while chunk := f.read(8192):
+            hasher.update(chunk)
+    return hasher.hexdigest()
